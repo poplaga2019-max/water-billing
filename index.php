b57553df239faacab8876cbe472b35232ef8f152
poplaga2019-max <poplaga2019@gmail.com>
2026-03-20 15:54:36 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include 'config/db.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ระบบประปาหมู่บ้าน</title>
+</head>
+<body style="font-family: THSarabun, sans-serif;">
+    <h2>ระบบประปาหมู่บ้าน</h2>
+
+    <a href="login.php">เข้าสู่ระบบ</a>
+</body>
+</html>
