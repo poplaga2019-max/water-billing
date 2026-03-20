8f5ba267630150533e8fa8998425ff8a5ed2a8dd
poplaga2019-max <poplaga2019@gmail.com>
2026-03-20 16:02:48 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -11,3 +11,15 @@ CREATE TABLE users (
 INSERT INTO users (username, password, role) VALUES
 ('admin', '1234', 'admin'),
 ('staff', '1234', 'staff');
+
+CREATE TABLE customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    address TEXT,
+    meter_no VARCHAR(50),
+    last_unit INT DEFAULT 0
+);
+
+INSERT INTO customers (name, address, meter_no, last_unit) VALUES
+('สมชาย', 'บ้านเลขที่ 12', 'M001', 120),
+('สมหญิง', 'บ้านเลขที่ 15', 'M002', 90);
