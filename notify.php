4c898be785f62b455b8c0c1798ad0fec3407de00
poplaga2019-max <poplaga2019@gmail.com>
2026-03-24 22:15:32 +0700

    Update notify.php

--- notify.php
+++ notify.php
@@ -1,1 +1,22 @@
+<?php
+include 'config/db.php';
 
+function sendTelegram($msg, $type){
+
+    global $conn;
+
+    $set = $conn->query("SELECT * FROM settings WHERE id=1")->fetch_assoc();
+
+    $token = $set['telegram_token'];
+
+    if($type == 'meter'){
+        $chat_id = $set['telegram_meter'];
+    }else{
+        $chat_id = $set['telegram_payment'];
+    }
+
+    $url = "https://api.telegram.org/bot$token/sendMessage";
+
+    file_get_contents($url."?chat_id=$chat_id&text=".urlencode($msg));
+}
+?>
