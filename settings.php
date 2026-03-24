8a9a1a9b2884c9790d8e3efabb46a3319f3081c4
poplaga2019-max <poplaga2019@gmail.com>
2026-03-24 19:23:10 +0700

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,1 +1,35 @@
+<?php
+session_start();
+include 'config/db.php';
 
+$res = $conn->query("SELECT * FROM water_rates");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ตั้งค่าค่าน้ำ</title>
+</head>
+<body style="font-family: THSarabun, sans-serif;">
+
+<h2>ตั้งค่าค่าน้ำ (ขั้นบันได)</h2>
+
+<table border="1" cellpadding="5">
+<tr>
+    <th>หน่วยเริ่ม</th>
+    <th>หน่วยสุด</th>
+    <th>บาท/หน่วย</th>
+</tr>
+
+<?php while($r = $res->fetch_assoc()){ ?>
+<tr>
+    <td><?= $r['min_unit'] ?></td>
+    <td><?= $r['max_unit'] ?></td>
+    <td><?= $r['price_per_unit'] ?></td>
+</tr>
+<?php } ?>
+
+</table>
+
+</body>
+</html>
