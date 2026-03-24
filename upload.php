2615f3d8241999a6f4a36de6a3221b86b51050c1
poplaga2019-max <poplaga2019@gmail.com>
2026-03-24 19:50:20 +0700

    Update upload.php
    <?php
    include 'config/db.php';
    
    $id = $_POST['id'];
    
    $target = "uploads/" . time() . $_FILES['slip']['name'];
    move_uploaded_file($_FILES['slip']['tmp_name'], $target);
    
    $conn->query("UPDATE bills SET slip='$target' WHERE id=$id");
    
    header("Location: mybill.php");

--- upload.php
+++ upload.php
@@ -1,1 +1,11 @@
+<?php
+include 'config/db.php';
 
+$id = $_POST['id'];
+
+$target = "uploads/" . time() . $_FILES['slip']['name'];
+move_uploaded_file($_FILES['slip']['tmp_name'], $target);
+
+$conn->query("UPDATE bills SET slip='$target' WHERE id=$id");
+
+header("Location: mybill.php");
