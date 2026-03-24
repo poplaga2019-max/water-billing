bd75f992646a66707da9167e24e1c0ab86881caf
poplaga2019-max <poplaga2019@gmail.com>
2026-03-24 21:30:07 +0700

    Update thsarabunnew.php

--- tcpdf/fonts/thsarabunnew.php
+++ tcpdf/fonts/thsarabunnew.php
@@ -1,1 +1,21 @@
-
+<?php
+$type = 'TrueTypeUnicode';
+$name = 'THSarabunNew';
+$desc = array(
+    'Ascent' => 1000,
+    'Descent' => -200,
+    'CapHeight' => 700,
+    'Flags' => 32,
+    'FontBBox' => '[-500 -200 1500 1000]',
+    'ItalicAngle' => 0,
+    'StemV' => 70,
+    'MissingWidth' => 600
+);
+$up = -75;
+$ut = 50;
+$cw = array();
+$enc = '';
+$diff = '';
+$file = 'thsarabunnew.z';
+$originalsize = 0;
+?>
