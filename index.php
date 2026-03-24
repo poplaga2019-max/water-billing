5fdbfeb30b93284d9ecd365e44e5522316951aa7
poplaga2019-max <poplaga2019@gmail.com>
2026-03-24 20:32:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,14 @@ include 'config/db.php';
     <title>ระบบประปาหมู่บ้าน</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body style="font-family: THSarabun, sans-serif;">
-    <h2>ระบบประปาหมู่บ้าน</h2>
+<body class="container mt-5 text-center" style="font-family: THSarabun, sans-serif;">
 
-    <a href="login.php">เข้าสู่ระบบ</a>
-<a href="customers.php">เข้าระบบจดมิเตอร์</a>
+<h2 class="mb-4">💧 ระบบประปาหมู่บ้าน</h2>
+
+<div class="d-grid gap-3 col-6 mx-auto">
+    <a href="login.php" class="btn btn-primary btn-lg">เข้าสู่ระบบ</a>
+    <a href="customers.php" class="btn btn-success btn-lg">จดมิเตอร์น้ำ</a>
+</div>
+
 </body>
 </html>
