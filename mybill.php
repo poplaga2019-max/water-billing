600e582ade5a5e4c1df5226a844e8c9ee247c1f9
poplaga2019-max <poplaga2019@gmail.com>
2026-03-24 20:20:43 +0700

    Update mybill.php

--- mybill.php
+++ mybill.php
@@ -23,7 +23,7 @@ ORDER BY id DESC
 <body style="font-family: THSarabun, sans-serif;">
 
 <h2>บิลของฉัน</h2>
-
+<p>โอนเงินได้ที่: 080-xxx-xxxx</p>
 <table border="1" cellpadding="5">
 <tr>
     <th>หน่วยใช้</th>
