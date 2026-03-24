2c8126e15a42a215e1ba2acae7f28848369d82e8
poplaga2019-max <poplaga2019@gmail.com>
2026-03-24 20:23:52 +0700

    Update mybill.php

--- mybill.php
+++ mybill.php
@@ -23,6 +23,8 @@ ORDER BY id DESC
 <body style="font-family: THSarabun, sans-serif;">
 
 <h2>บิลของฉัน</h2>
+    <img src="https://promptpay.io/0801234567.png" width="200">
+<p>สแกน QR เพื่อชำระเงิน</p>
 <p>โอนเงินได้ที่: 080-xxx-xxxx</p>
 <table border="1" cellpadding="5">
 <tr>
