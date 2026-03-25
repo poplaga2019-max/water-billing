018fda37e54b66a0dbaf9d93231be3c5471c7dcb
poplaga2019-max <poplaga2019@gmail.com>
2026-03-25 15:55:29 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,13 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "water_db";
+$conn = new mysqli("localhost","root","","water_db");
 
-$conn = new mysqli($host, $user, $pass, $db);
-$conn->set_charset("utf8");
-
-if ($conn->connect_error) {
-    die("เชื่อมต่อฐานข้อมูลไม่ได้");
+if($conn->connect_error){
+    die("DB Error");
 }
 ?>
