0fcdd25ee4e84ba1b35efd490703dd883f9f6167
poplaga2019-max <poplaga2019@gmail.com>
2026-03-25 20:43:58 +0700

    Update layout_footer.php

--- layout_footer.php
+++ layout_footer.php
@@ -1,1 +1,8 @@
+</div>
 
+<div class="bottom-menu">
+    <a href="dashboard.php">🏠<br>หน้าแรก</a>
+    <a href="customers.php">👤<br>ลูกบ้าน</a>
+    <a href="meter.php">💧<br>จด</a>
+    <a href="bills.php">🧾<br>บิล</a>
+</div>
